5d97d2ac305db097d9accd05dad5a8062f533d10
MelnykRoman6 <melnykromandev@gmail.com>
2026-01-05 04:02:22 +0100

    removed direct token assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import API.WitcherApiClient;
+import lombok.Value;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
@@ -8,9 +13,21 @@ public class Main {
 
         WitcherApiClient apiClient = new WitcherApiClient();
 
+        Properties prop = new Properties();
+        String botToken = "";
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("");
+                return;
+            }
 
+            prop.load(input);
 
-        String botToken = "8002882388:AAG-tpVMyAdrx0f7t5A151gj4r91WBNxtSw";
+            botToken = prop.getProperty("bot.token");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         botsApplication.registerBot(botToken, new Bot(botToken));
 
         System.out.println("Bot successfully started!");
